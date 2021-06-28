24b9aaae1a09a9c5ba865608352e6230209aa3d3
Shobhit Sundriyal <35799284+shobhitsundriyal@users.noreply.github.com>
2021-06-28 15:33:01 +0530

    Create temp.cpp

---
+++ temp.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+
+	// code goes here
+	int a, b;
+	cin>>a>>b;
+	cout<<a+b;
+
+	return 0;
+}
